4b0014b6da952ae37a955d56038503d99d6a302d
Sylar490 <algandar490@gmail.com>
2025-10-22 07:59:48 -0700

    Emepzamos algo para la sesion y los 3 mensajes

--- src/ClienteMulti/ParaMandar.java
+++ src/ClienteMulti/ParaMandar.java
@@ -9,8 +9,23 @@ import java.net.Socket;
 public class ParaMandar implements Runnable {
     final BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
     final DataOutputStream salida ;
+    private boolean sesionIniciada = false;
+    private String username ;
+    private byte mensajesEnviados =0;
+    private byte LimiteMensajes =3;
+
     public ParaMandar(Socket s) throws IOException {
         this.salida = new DataOutputStream(s.getOutputStream());
+    }
+
+    private void mostrarAyuda(){
+        System.out.println("""
+                comandos Disponibles:
+                /register -Usuario- -Contraseña- -Para reguistrar se
+                /login -Usuario- -Contraseña- -Para iniciar sesion
+                /help -Muestra los comandos disponibles
+                Nota: si no tienes sesion activa solo puedes mandar 3 mensajes, Cuidado con eso
+                """);
     }
 
     @Override
@@ -19,7 +34,10 @@ public class ParaMandar implements Runnable {
             String mensaje;
             try {
                 mensaje = teclado.readLine();
-                salida.writeUTF(mensaje);
+                if (mensaje.startsWith("/")){
+                    //ProcesarComando(Mensaje);
+                    //continue;
+                }
             } catch (IOException ex) {
             }
 
