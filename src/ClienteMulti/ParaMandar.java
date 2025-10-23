86c56d02aec42e073eec6e3ff6b8776b1b013ac0
Sylar490 <algandar490@gmail.com>
2025-10-23 07:39:26 -0700

    Arganizando comandos

--- src/ClienteMulti/ParaMandar.java
+++ src/ClienteMulti/ParaMandar.java
@@ -18,15 +18,6 @@ public class ParaMandar implements Runnable {
         this.salida = new DataOutputStream(s.getOutputStream());
     }
 
-    private void mostrarAyuda(){
-        System.out.println("""
-                comandos Disponibles:
-                /register -Usuario- -Contraseña- -Para reguistrar se
-                /login -Usuario- -Contraseña- -Para iniciar sesion
-                /help -Muestra los comandos disponibles
-                Nota: si no tienes sesion activa solo puedes mandar 3 mensajes, Cuidado con eso
-                """);
-    }
 
     @Override
     public void run() {
@@ -35,8 +26,7 @@ public class ParaMandar implements Runnable {
             try {
                 mensaje = teclado.readLine();
                 if (mensaje.startsWith("/")){
-                    //ProcesarComando(Mensaje);
-                    //continue;
+
                 }
             } catch (IOException ex) {
             }
