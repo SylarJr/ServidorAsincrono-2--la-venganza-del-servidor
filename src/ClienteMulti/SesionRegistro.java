ce3661713e66d52813803b0fbea61144a1755da5
Sylar490 <algandar490@gmail.com>
2025-10-26 18:08:50 -0700

    Registros Funcionales

--- src/ClienteMulti/SesionRegistro.java
+++ src/ClienteMulti/SesionRegistro.java
@@ -5,15 +5,15 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 public class SesionRegistro {
    
-    private static final String DB_URL = "jdbc:sqlite:C:\\Users\\Juan\\Documents\\Proyectos\\Servidor Asincrono\\ServidorAsincrono\\src\\ClienteMulti\\Usuarios.db";
+    private static final String DB_URL = "jdbc:sqlite:C:/Users/Juan/Documents/Proyectos/Servidor Asincrono/ServidorAsincrono/src/ClienteMulti/Usuarios.db";
     
-    public static boolean registrarUsuario(String username, String password) {
-        String sql = "INSERT INTO Usuarios(username, password) VALUES(?,?)";
+    public static boolean registrarUsuario(String Usuario, String Contraseña) {
+        String sql = "INSERT INTO Usuarios(Usuario, Contraseña) VALUES(?,?)";
 
         try (Connection conn = DriverManager.getConnection(DB_URL);
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, username);
-            pstmt.setString(2, password);
+            pstmt.setString(1, Usuario);
+            pstmt.setString(2, Contraseña);
             pstmt.executeUpdate();
             return true;
         } catch (SQLException e) {
@@ -22,12 +22,12 @@ public class SesionRegistro {
         }
     }
 
-    public static boolean iniciarSesion(String username, String password) {
-        String sql = "SELECT * FROM Usuarios WHERE username = ? AND password = ?";
+    public static boolean iniciarSesion(String Usuario, String Contraseña) {
+        String sql = "SELECT * FROM Usuarios WHERE Usuario = ? AND Contraseña = ?";
         try (Connection conn = DriverManager.getConnection(DB_URL);
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, username);
-            pstmt.setString(2, password);
+            pstmt.setString(1, Usuario);
+            pstmt.setString(2, Contraseña);
             return pstmt.executeQuery().next();
         } catch (SQLException e) {
             System.out.println("Error al iniciar sesion: " + e.getMessage());
